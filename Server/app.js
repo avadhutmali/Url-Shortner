b61af7a8209ad442bb6c2d3c7f6038252bbdd44d
Avadhut <mali.avadhut7@gmail.com>
2024-12-28 02:14:20 +0530

    Mongo_url to mongo_uri

--- Server/app.js
+++ Server/app.js
@@ -9,10 +9,10 @@ dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URI = process.env.MONGO_URI
 
-connectMongoose(MONGO_URL)
-    .then(()=>{console.log("MongoDB is connected to url: "+MONGO_URL)})
+connectMongoose(MONGO_URI)
+    .then(()=>{console.log("MongoDB is connected to url: "+MONGO_URI)})
     
 app.use(cors())
 app.use(express.json())
@@ -22,5 +22,5 @@ app.use(express.urlencoded({extended:true}))
 app.use("/api/url",routeUrl)
 app.use("/api/user",routeUser)
 
-app.listen(PORT,()=>{console.log("Servver is starting at port: "+PORT)})
+app.listen(PORT,()=>{console.log("Server is starting at port: "+PORT)})
 
