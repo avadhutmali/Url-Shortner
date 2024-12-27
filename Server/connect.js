b4fefd92aec2b29ecfd2d96f485d811db4f28735
Avadhut <mali.avadhut7@gmail.com>
2024-12-28 03:04:20 +0530

    mongoose

--- Server/connect.js
+++ Server/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 async function connectMongoose(url){
-    return mongoose.connect(url)
+    return mongoose.connect("mongodb+srv://maliavadhut7:5hP7NiRAx0AgLk4N@cluster0.lz3jb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 }
 
 module.exports = {connectMongoose}
